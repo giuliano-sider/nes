099a333c0f7aab6d99ad53c0db118fa0712911a5
Alan Ricardo Gomes <alan.gomes@thoughtworks.com>
2019-08-26 00:23:34 -0300

    [Alan] infinite beep added to dummy game

---
+++ helloworld.asm
@@ -0,0 +1,81 @@
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   ;MyVariable0 .dsb 1
+   ;MyVariable1 .dsb 3
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   ;.enum $0100
+   ;.ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+Reset:
+
+   ;NOTE: initialization code goes here
+lda #%00000001
+sta $4015 ;enable square 1
+
+lda #%10111111 ;Duty 10, Volume F
+sta $4000
+
+lda #$C9    ;0C9 is a C# in NTSC mode
+sta $4002
+lda #$00
+sta $4003
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
+
+;----------------------------------------------------------------
+; CHR-ROM bank
+;----------------------------------------------------------------
+
+   ;.incbin "tiles.chr"
