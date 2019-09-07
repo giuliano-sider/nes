5f0ff67d457d15e98a7dde3c55dcc3fd3b7e5fe1
Carlos Robson <ra135257@students.ic.unicamp.br>
2019-09-06 21:58:36 -0300

    Log and main emulator files

--- Emulator/CPU/log.py
+++ Emulator/CPU/log.py
@@ -1,11 +1,10 @@
 
-
 def printLog(regPC, regA, regX, regY, regSP,  regP):
-	print("| pc = 0x%04x | a = 0x%04x  | x = 0x%04x | y = 0x%04x | sp = 0x%04x | p[NV-BDIZC] = 10101010 |" % (regPC 
-	,regA, regX, regY, regSP ))
-	
-def printLogs(regPC, regA, regX, regY, regSP,  regP, memAddr, data):
-	print("| pc = 0x%04x | a = 0x%04x  | x = 0x%04x | y = 0x%04x | sp = 0x%04x | p[NV-BDIZC] = 10101010 | MEM[0x%04x] = 0x99 |" % (regPC 
-	,regA, regX, regY, regSP ))
+	print("| pc = 0x%04x | a = 0x%04x  | x = 0x%04x | y = 0x%04x | sp = 0x%04x | p[NV-BDIZC] = %s |" % (regPC 
+	,regA, regX, regY, regSP, format(14, regP)))
 
-printLog(1,1,1,1,0xFFC,0);
+def printLogs(regPC, regA, regX, regY, regSP,  regP, memAddr, data):
+	print("| pc = 0x%04x | a = 0x%04x  | x = 0x%04x | y = 0x%04x | sp = 0x%04x | p[NV-BDIZC] = %d | MEM[0x%04x] = 0x%04x |" % (regPC 
+	,regA, regX, regY, regSP, regP, memAddr, data))
+
+printLog(1,1,1,1,0xFFC,'08b');
