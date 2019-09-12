43ca6d81f3bac55afd3db639ada445447eb0e427
giuliano <giuliano.sider@gmail.com>
2019-09-11 23:44:05 -0300

    Add an example of a unit test of a NES CPU instruction using the CPU API and the NES test utilities.

---
+++ tst/test_arithmetic.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import unittest
+
+sys.path += os.pardir
+from nes_cpu_test_utils import CreateTestCpu, execute_instruction
+from instructions import ADC_IMMEDIATE
+
+class TestArithmetic(unittest.TestCase):
+
+    def test_adc_immediate_simple_addition_with_no_flag_setting(self):
+        cpu = CreateTestCpu()
+        cpu.clear_carry()
+        cpu.set_A(0x00)
+
+        # ADC #$10
+        execute_instruction(cpu, opcode=ADC_IMMEDIATE, op2_lo_byte=0x10)
+        
+        self.assertEqual(cpu.A(), 0x10)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.overflow(), 0)
+        self.assertEqual(cpu.zero(), 0)
+        self.assertEqual(cpu.carry(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
