ee8b4084390c651d5e70650953e4ee9f9145001e
Alan Ricardo Gomes <alan.gomes@thoughtworks.com>
2019-09-13 21:00:48 -0300

    added constant for load and store instructions

--- Instructions/load_instructions.py
+++ Instructions/load_instructions.py
@@ -29,55 +29,69 @@ def lda_indirect_x(cpu, logger):
     # to be implemented OPCODE a1
     raise NotImplementedError()
 
+LDA_ABSOLUTE_Y = 0xB9
 def lda_absolute_y(cpu, logger):
     # to be implemented OPCODE b9
     raise NotImplementedError()
 
+LDA_ABSOLUTE_X = 0xBD
 def lda_absolute_x(cpu, logger):
     # to be implemented OPCODE bd
     raise NotImplementedError()
 
+LDA_ZEROPAGE_X = 0xB5
 def lda_zeropage_x(cpu, logger):
     # to be implemented OPCODE b5
     raise NotImplementedError()
 
+LDX_IMMEDIATE = 0xA2
 def ldx_immediate(cpu, logger):
     # to be implemented OPCODE a2
     raise NotImplementedError()
 
+LDX_ZEROPAGE = 0xA6
 def ldx_zeropage(cpu, logger):
     # to be implemented OPCODE a6
     raise NotImplementedError()
 
 
+LDX_ABSOLUTE = 0xAE
 def ldx_absolute(cpu, logger):
     # to be implemented OPCODE ae
     raise NotImplementedError()
 
+
+LDX_ZEROPAGE_Y = 0xB6
 def ldx_zeropage_y(cpu, logger):
     # to be implemented OPCODE b6
     raise NotImplementedError()
 
+LDX_ABSOLUTE_Y = 0xBE
 def ldx_absolute_y(cpu, logger):
     # to be implemented OPCODE be
     raise NotImplementedError()
 
+LDY_IMMEDIATE = 0xA0
 def ldy_immediate(cpu, logger):
     # to be implemented OPCODE a0
     raise NotImplementedError()
 
+LDY_ZEROPAGE = 0xA4
 def ldy_zeropage(cpu, logger):
     # to be implemented OPCODE a4
     raise NotImplementedError()
 
+LDY_ABSOLUTE = 0xAC
 def ldy_absolute(cpu, logger):
     # to be implemented OPCODE ac
     raise NotImplementedError()
 
+LDY_ZEROPAGE_X = 0xB4
 def ldy_zeropage_x(cpu, logger):
     # to be implemented OPCODE b4
     raise NotImplementedError()
 
+LDY_ABSOLUTE_X = 0xBC
 def ldy_absolute_x(cpu, logger):
     # to be implemented OPCODE bc
     raise NotImplementedError()
--- Instructions/store_instructions.py
+++ Instructions/store_instructions.py
@@ -1,51 +1,64 @@
+STA_INDIRECT_X = 0x81
 def sta_indirect_x(cpu, logger):
     # to be implemented OPCODE 81
     raise NotImplementedError()
 
+STA_ZEROPAGE = 0x85
 def sta_zeropage(cpu, logger):
     # to be implemented OPCODE 85
     raise NotImplementedError()
 
+STA_ABSOLUTE = 0xBD
 def sta_absolute(cpu, logger):
     # to be implemented OPCODE 8d
     raise NotImplementedError()
 
+STA_INDIRECT_Y = 0x91
 def sta_indirect_y(cpu, logger):
     # to be implemented OPCODE 91
     raise NotImplementedError()
 
+STA_ZEROPAGE_X = 0x95
 def sta_zeropage_x(cpu, logger):
     # to be implemented OPCODE 95
     raise NotImplementedError()
 
+STA_ABSOLUTE_Y = 0x99
 def sta_absolute_y(cpu, logger):
     # to be implemented OPCODE 99
     raise NotImplementedError()
 
+STA_ABSOLUTE_X = 0x9D
 def sta_absolute_x(cpu, logger):
     # to be implemented OPCODE 9d
     raise NotImplementedError()
 
+STY_ZEROPAGE = 0x84
 def sty_zeropage(cpu, logger):
     # to be implemented OPCODE 84
     raise NotImplementedError()
 
+STY_ABSOLUTE = 0x8C
 def sty_absolute(cpu, logger):
     # to be implemented OPCODE 8c
     raise NotImplementedError()
 
+STY_ABSOLUTE_X = 0x94
 def sty_zeropage_x(cpu, logger):
     # to be implemented OPCODE 94
     raise NotImplementedError()
 
+STX_ZEROPAGE = 0x86
 def stx_zeropage(cpu, logger):
     # to be implemented OPCODE 86
     raise NotImplementedError()
 
+STX_ABSOLUTE = 0x8E
 def stx_absolute(cpu, logger):
     # to be implemented OPCODE 8e
     raise NotImplementedError()
 
+STX_ZEROPAGE_X = 0x96
 def stx_zeropage_x(cpu, logger):
     # to be implemented OPCODE 96
     raise NotImplementedError()
