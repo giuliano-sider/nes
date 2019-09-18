2c7a62c6483fc6be48dcd128e9973407a24668a6
giuliano <giuliano.sider@gmail.com>
2019-09-18 19:02:21 -0300

    Add implementation of BIT_ZEROPAGE.

--- Instructions/logical_instructions.py
+++ Instructions/logical_instructions.py
@@ -1,4 +1,11 @@
+from Instructions.address_getters import get_zeropage
+
+
 BIT_ZEROPAGE = 0x24
 def bit_zeropage(cpu, logger):
-    # to be implemented OPCODE 24
-    raise NotImplementedError()
+    op2 = get_zeropage(cpu)
+    cpu.set_A(cpu.A() & op2)
+
+    cpu.set_negative_iff(op2 & 0b10000000)
+    cpu.set_overflow_iff(op2 & 0b01000000)
+    cpu.set_zero_iff(cpu.A() == 0)
