60ddd4b80378e18a70375f65330944658523681e
Alan Ricardo Gomes <alan.gomes@thoughtworks.com>
2019-09-20 22:42:06 -0300

    add sta absolute x

--- tst/test_store.py
+++ tst/test_store.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 from nes_cpu_test_utils import CreateTestCpu, execute_instruction
-from instructions import STA_INDIRECT_X, STA_ZEROPAGE, STA_ABSOLUTE, STA_INDIRECT_Y, STA_ZEROPAGE_X
+from instructions import STA_INDIRECT_X, STA_ZEROPAGE, STA_ABSOLUTE, STA_INDIRECT_Y, STA_ZEROPAGE_X, STA_ABSOLUTE_Y
 
 
 class TestStore(unittest.TestCase):
@@ -164,6 +164,29 @@ class TestStore(unittest.TestCase):
 
         self.assertEqual(self.cpu.memory[resolved_address], value_to_be_stored)
         self.assertEqual(self.cpu.PC(), expected_pc)
+
+    def test_sta_absolute_y(self):
+        initial_pc = self.cpu.PC()
+        y_value = 0x01
+        value_to_be_stored = 0x10
+        lo_stored_address = 0x00
+        hi_stored_address = 0x02
+        resolved_address = lo_stored_address + (hi_stored_address << 8) + y_value
+
+        self.cpu.set_Y(y_value)
+        self.cpu.set_A(value_to_be_stored)
+        execute_instruction(
+            self.cpu,
+            opcode=STA_ABSOLUTE_Y,
+            op2_lo_byte=lo_stored_address,
+            op2_hi_byte=hi_stored_address
+        )
+
+        expected_pc = initial_pc + 3
+
+        self.assertEqual(self.cpu.memory[resolved_address], value_to_be_stored)
+        self.assertEqual(self.cpu.PC(), expected_pc)
+
 
 if __name__ == '__main__':
     unittest.main()
