9a9af59c058f2d0d4f40378afdbf150445920a02
giuliano <giuliano.sider@gmail.com>
2019-09-22 12:24:09 -0300

    Complete tests for the ORA and EOR variants.

--- tst/test_logical.py
+++ tst/test_logical.py
@@ -148,6 +148,99 @@ class TestLogicalInstructions(unittest.TestCase):
         self.assertEqual(cpu.negative(), 0)
         self.assertEqual(cpu.zero(), 0)
 
+    def test_ORA_immediate(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_IMMEDIATE, op2_lo_byte=0b00110000)
+
+        self.assertEqual(cpu.A(), 0b00110001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_zeropage(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.memory[0x11] =   0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_ZERO_PAGE, op2_lo_byte=0x11)
+
+        self.assertEqual(cpu.A(), 0b00110001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_zeropage_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x20)
+        cpu.memory[0x31] =   0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_ZERO_PAGE_X, op2_lo_byte=0x11)
+
+        self.assertEqual(cpu.A(), 0b00110001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_absolute_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x10)
+        cpu.memory[0x1021] = 0b00010000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_ABSOLUTE_X, op2_lo_byte=0x11, op2_hi_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00010001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_absolute_y(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_Y(0x10)
+        cpu.memory[0x1021] = 0b00010000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_ABSOLUTE_Y, op2_lo_byte=0x11, op2_hi_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00010001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_indirect_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x20)
+        cpu.memory[0x30] = 0x34
+        cpu.memory[0x31] = 0x12
+        cpu.memory[0x1234] = 0b00010000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_INDIRECT_X, op2_lo_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00010001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_ORA_indirect_y(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(                  0b00010001)
+        cpu.set_Y(0x20)
+        cpu.memory[0x10] = 0x34
+        cpu.memory[0x11] = 0x12
+        cpu.memory[0x1234 + 0x20] = 0b00010000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=ORA_INDIRECT_Y, op2_lo_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00010001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+
     def test_EOR_absolute(self):
         cpu = CreateTestCpu()
         cpu.set_A(           0b00010001)
@@ -159,6 +252,99 @@ class TestLogicalInstructions(unittest.TestCase):
         self.assertEqual(cpu.A(), 0b00000001)
         self.assertEqual(cpu.negative(), 0)
         self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_immediate(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_IMMEDIATE, op2_lo_byte=0b00110000)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_zeropage(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.memory[0x11] =   0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_ZERO_PAGE, op2_lo_byte=0x11)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_zeropage_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x20)
+        cpu.memory[0x31] =   0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_ZERO_PAGE_X, op2_lo_byte=0x11)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_absolute_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x10)
+        cpu.memory[0x1021] = 0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_ABSOLUTE_X, op2_lo_byte=0x11, op2_hi_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_absolute_y(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_Y(0x10)
+        cpu.memory[0x1021] = 0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_ABSOLUTE_Y, op2_lo_byte=0x11, op2_hi_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_indirect_x(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(           0b00010001)
+        cpu.set_X(0x20)
+        cpu.memory[0x30] = 0x34
+        cpu.memory[0x31] = 0x12
+        cpu.memory[0x1234] = 0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_INDIRECT_X, op2_lo_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
+    def test_EOR_indirect_y(self):
+        cpu = CreateTestCpu()
+        cpu.set_A(                  0b00010001)
+        cpu.set_Y(0x20)
+        cpu.memory[0x10] = 0x34
+        cpu.memory[0x11] = 0x12
+        cpu.memory[0x1234 + 0x20] = 0b00110000
+        cpu.set_P(0xFF)
+
+        execute_instruction(cpu, opcode=EOR_INDIRECT_Y, op2_lo_byte=0x10)
+
+        self.assertEqual(cpu.A(), 0b00100001)
+        self.assertEqual(cpu.negative(), 0)
+        self.assertEqual(cpu.zero(), 0)
+
 
     def test_ASL_absolute(self):
         cpu = CreateTestCpu()
