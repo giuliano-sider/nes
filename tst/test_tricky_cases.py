da1b295736fd28c4660da81e217600520e4f4f83
giuliano <giuliano.sider@gmail.com>
2019-09-27 14:10:18 -0300

    Add a tricky test of JMP indirect behavior.

--- tst/test_tricky_cases.py
+++ tst/test_tricky_cases.py
@@ -4,7 +4,7 @@ import unittest
 
 sys.path += os.pardir
 from nes_cpu_test_utils import CreateTestCpu, execute_instruction
-from instructions import JSR
+from instructions import JSR, JMP_INDIRECT
 from memory_mapper import MEMORY_SIZE
 
 class TestTrickyCases(unittest.TestCase):
@@ -22,10 +22,16 @@ class TestTrickyCases(unittest.TestCase):
 
     # TODO: def test_stack_page_wraparound_when_pushing_PC_with_a_jsr(self):
 
-    # TODO: def test_that_jmp_indirect_addr_wrapsaround_in_the_same_page(self):
+    def test_that_jmp_indirect_addr_wrapsaround_in_the_same_page(self):
+        cpu = CreateTestCpu()
+        cpu.set_PC(0x200)
+        cpu.memory[0x1FF] = 0x34
+        cpu.memory[0x200] = 0x56 # wrong high byte
+        cpu.memory[0x100] = 0x12 # correct high byte
 
+        execute_instruction(cpu, opcode=JMP_INDIRECT, op2_lo_byte=0xFF, op2_hi_byte=0x01)
 
-
+        self.assertEqual(cpu.PC(), 0x1234)
 
 if __name__ == '__main__':
     unittest.main()
