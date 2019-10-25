e9e8fe298ae0bedcb14fa62fdcd02fa8143e37b7
Carlos Robson <ra135257@students.ic.unicamp.br>
2019-10-25 20:12:01 -0300

    Clock ticks for all arithmetics

--- Instructions/arithmetics_instructions.py
+++ Instructions/arithmetics_instructions.py
@@ -78,12 +78,14 @@ def cmp_immediate(cpu, logger):
     cpu.set_zero_iff(op1 == op2)
     cpu.set_carry_iff(op1 >= op2)
     cpu.set_negative_iff(is_negative(result))
+    cpu.clock_ticks_since_reset += 2
     logger.log_instruction(cpu)
 
 CMP_ZEROPAGE = 0xC5
 def cmp_zeropage(cpu, logger):
     op1 = cpu.A()
     op2 = get_zeropage_addr(cpu)
+    cpu.clock_ticks_since_reset += 3
     cmp(cpu, logger, op1, op2)
 
 
@@ -91,19 +93,22 @@ CMP_ZEROPAGE_X = 0xD5
 def cmp_zero_page_x(cpu, logger):
     op1 = cpu.A()
     op2 = get_zeropage_x_addr(cpu)
+    cpu.clock_ticks_since_reset += 4
     cmp(cpu, logger, op1, op2)
 
 CMP_ABSOLUTE = 0xCD
 def cmp_absolute(cpu, logger):
     op1 = cpu.A()
     op2 = get_absolute_addr(cpu)
+    cpu.clock_ticks_since_reset += 4
     cmp(cpu, logger, op1, op2)
 
 
 CMP_ABSOLUTE_X = 0xDD
 def cmp_absolute_x(cpu, logger):
     op1 = cpu.A()
-    op2, crosspage = get_absolute_x_addr(cpu)
+    op2, pageCrossed = get_absolute_x_addr(cpu)
+    cpu.clock_ticks_since_reset += 4 + pageCrossed
     cmp(cpu, logger, op1, op2)
 
 CMP_ABSOLUTE_Y = 0xD9
@@ -117,6 +122,7 @@ CMP_INDIRECT_X = 0xC1
 def cmp_indirect_x(cpu, logger):
     op1 = cpu.A()
     op2 = get_indirect_x_addr(cpu)
+    cpu.clock_ticks_since_reset += 6
     cmp(cpu, logger, op1, op2)
 
 CMP_INDIRECT_Y = 0xD1
@@ -134,18 +140,21 @@ def cpx_immediate(cpu, logger):
     cpu.set_zero_iff(op1 == op2)
     cpu.set_carry_iff(op1 >= op2)
     cpu.set_negative_iff(is_negative(result))
+    cpu.clock_ticks_since_reset += 2
     logger.log_instruction(cpu)
 
 CPX_ZEROPAGE = 0xE4
 def cpx_zeropage(cpu, logger):
     op1 = cpu.X()
     op2 = get_zeropage_addr(cpu)
+    cpu.clock_ticks_since_reset += 3
     cmp(cpu, logger, op1, op2)
 
 CPX_ABSOLUTE = 0xEC
 def cpx_absolute(cpu, logger):
     op1 = cpu.X()
     op2 = get_absolute_addr(cpu)
+    cpu.clock_ticks_since_reset += 4
     cmp(cpu, logger, op1, op2)
 
 CPY_IMMEDIATE = 0xC0
@@ -156,18 +165,21 @@ def cpy_immediate(cpu, logger):
     cpu.set_zero_iff(op1 == op2)
     cpu.set_carry_iff(op1 >= op2)
     cpu.set_negative_iff(is_negative(result))
+    cpu.clock_ticks_since_reset += 2
     logger.log_instruction(cpu)
 
 CPY_ZEROPAGE = 0xC4
 def cpy_zeropage(cpu, logger):
     op1 = cpu.Y()
     op2 = get_zeropage_addr(cpu)
+    cpu.clock_ticks_since_reset += 3
     cmp(cpu, logger, op1, op2)
 
 CPY_ABSOLUTE = 0xCC
 def cpy_absolute(cpu, logger):
     op1 = cpu.Y()
     op2 = get_absolute_addr(cpu)
+    cpu.clock_ticks_since_reset += 4
     cmp(cpu, logger, op1, op2)
 
 def cmp(cpu, logger, op1, addr):
@@ -178,26 +190,28 @@ def cmp(cpu, logger, op1, addr):
     cpu.set_negative_iff(is_negative(result))
     logger.log_memory_access_instruction(cpu, addr, op2)
 
-
-
 DEC_ZEROPAGE = 0XC6
 def dec_zeropage(cpu, logger):
     addr = get_zeropage_addr(cpu)
+    cpu.clock_ticks_since_reset += 5
     dec(cpu, logger, addr)
 
 DEC_ZEROPAGE_X = 0xD6
 def dec_zeropage_x(cpu, logger):
   addr = get_zeropage_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 6
   dec(cpu, logger, addr)
 
 DEC_ABSOLUTE = 0xCE
 def dec_absolute(cpu, logger):
   addr = get_absolute_addr(cpu)
+  cpu.clock_ticks_since_reset += 6
   dec(cpu, logger, addr)
 
 DEC_ABSOLUTE_X = 0xDE
 def dec_absolute_x(cpu, logger):
-  addr, crosspage = get_absolute_x_addr(cpu)
+  addr, pageCrossed = get_absolute_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 7
   dec(cpu, logger, addr)
 
 DEX = 0xCA
@@ -206,6 +220,7 @@ def dex(cpu, logger):
     cpu.set_X(result)
     cpu.set_zero_iff(result == 0)
     cpu.set_negative_iff(is_negative(result))
+    cpu.clock_ticks_since_reset += 2
     logger.log_instruction(cpu)
 
 DEY = 0x88
@@ -214,6 +229,7 @@ def dey(cpu, logger):
   cpu.set_Y(result)
   cpu.set_zero_iff(result == 0)
   cpu.set_negative_iff(is_negative(result))
+  cpu.clock_ticks_since_reset += 2
   logger.log_instruction(cpu)
 
 
@@ -228,21 +244,25 @@ def dec(cpu, logger, addr):
 INC_ZEROPAGE = 0xE6
 def inc_zeropage(cpu, logger):
   addr = get_zeropage_addr(cpu)
+  cpu.clock_ticks_since_reset += 5
   inc(cpu, logger, addr)
 
 INC_ZEROPAGE_X = 0xF6
 def inc_zeropage_x(cpu, logger):
   addr = get_zeropage_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 6
   inc(cpu, logger, addr)
 
 INC_ABSOLUTE = 0xEE
 def inc_absolute(cpu, logger):
   addr = get_absolute_addr(cpu)
+  cpu.clock_ticks_since_reset += 6
   inc(cpu, logger, addr)
 
 INC_ABSOLUTE_X = 0xFE
 def inc_absolute_x(cpu, logger):
-  addr, crosspage = get_absolute_x_addr(cpu)
+  addr, pageCrossed = get_absolute_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 7
   inc(cpu, logger, addr)
 
 INX = 0xE8
@@ -252,6 +272,7 @@ def inx(cpu, logger):
   cpu.set_zero_iff(result == 0)
   cpu.set_negative_iff(is_negative(result))
   cpu.set_PC(cpu.PC() + 1)
+  cpu.clock_ticks_since_reset += 2
   logger.log_instruction(cpu)
 
 INY = 0xC8
@@ -261,6 +282,7 @@ def iny(cpu, logger):
   cpu.set_zero_iff(result == 0)
   cpu.set_negative_iff(is_negative(result))
   cpu.set_PC(cpu.PC() + 1)
+  cpu.clock_ticks_since_reset += 2
   logger.log_instruction(cpu)
 
 
@@ -283,32 +305,38 @@ def sbc_immediate(cpu, logger):
   cpu.set_overflow_iff(is_sbc_overflow(op1, op2, cpu.A()))
   cpu.set_zero_iff(cpu.A() == 0)
   cpu.set_carry_iff(result >= 0)
+  cpu.clock_ticks_since_reset += 2
   logger.log_instruction(cpu)
 
 
 SBC_ZEROPAGE = 0xE5
 def sbc_zeropage(cpu, logger):
   op2 = get_zeropage_addr(cpu)
+  cpu.clock_ticks_since_reset += 3
   sbc(cpu, logger, op2)
 
 SBC_ZEROPAGE_X = 0xF5
 def sbc_zeropage_x(cpu, logger):
   addr = get_zeropage_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 4
   sbc(cpu, logger, addr)
 
 SBC_ABSOLUTE = 0xED
 def sbc_absolute(cpu, logger):
   addr = get_absolute_addr(cpu)
+  cpu.clock_ticks_since_reset += 4
   sbc(cpu, logger, addr)
 
 SBC_ABSOLUTE_X = 0xFD
 def sbc_absolute_x(cpu, logger):
-  addr, crosspage = get_absolute_x_addr(cpu)
+  addr, pageCrossed = get_absolute_x_addr(cpu)
+  cpu.clock_ticks_since_reset += 4 + pageCrossed
   sbc(cpu, logger, addr)
 
 SBC_ABSOLUTE_Y = 0xF9
 def sbc_absolute_y(cpu, logger):
   addr, pageCrossed  = get_absolute_y_addr(cpu)
+  cpu.clock_ticks_since_reset += 4 + pageCrossed
   cpu.clock_ticks_since_reset += 4 + pageCrossed
   sbc(cpu, logger, addr)
 
@@ -321,6 +349,8 @@ def sbc_indirect_x(cpu, logger):
 SBC_INDIRECT_Y = 0xF1
 def sbc_indirect_y(cpu, logger):
   addr, pageCrossed = get_indirect_y_addr(cpu)
+    cpu.clock_ticks_since_reset += 5 + pageCrossed
+
   sbc(cpu, logger, addr)
 
 def sbc(cpu, logger, addr):
