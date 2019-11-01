113a0fa0201540974800df781a6492b47d5e56e6
Alan Ricardo Gomes <alan.gomes@thoughtworks.com>
2019-10-31 23:35:50 -0300

    added image pallete into rendering

--- nes.py
+++ nes.py
@@ -1,8 +1,28 @@
 from memory_mapper import MemoryMapper
 from cpu import Cpu
 from ppu import Ppu
+import numpy as np
+
+
+
 
 class Nes():
+
+    def given_pattern_table_in_address(self, ppu, address, tile):
+        current_address = address
+        for x in range(0, 8):
+            bit_string = ""
+            for y in range(0, 8):
+                bit_string = bit_string + str(tile[x][y])
+            content = int(bit_string, 2)
+            ppu.memory[current_address] = content
+            current_address = current_address + 1
+
+    def given_image_palette(self, ppu, image_palette):
+        palette_start_address = 0x3f00
+        for i in range(0, 16):
+            ppu.memory[palette_start_address + i] = image_palette[i]
+        return
 
     def __init__(self, iNES_file, test_mode=True):
         """Initialize a new Nes instance with the name of an iNES binary file or the binary contents of such a file."""
@@ -11,3 +31,5 @@ class Nes():
         self.ppu = Ppu(self.memory_mapper)
         self.memory_mapper.set_cpu(self.cpu)
         self.memory_mapper.set_ppu(self.ppu)
+        nes_image_palette = [0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15] # TO BE DELETED
+        self.given_image_palette(self.ppu, nes_image_palette) # TO BE DELETED
--- tst/test_ppu/test_render_background.py
+++ tst/test_ppu/test_render_background.py
@@ -362,24 +362,19 @@ class TestLoad(unittest.TestCase):
         self.given_image_palette(ppu, nes_image_palette)
         self.given_pattern_table_in_address(ppu, 0x0000, tile_pattern_lo_bit)
         self.given_pattern_table_in_address(ppu, 0x0008, tile_pattern_hi_bit)
-
-        zero_array = [0x75, 0x75, 0x75]
-        one_array = [0x27, 0x1b, 0x8f]
-        two_array = [0x00, 0x00, 0xab]
-        three_array = [0x47, 0x00, 0x9f]
         expected_tile = np.array([
-            [zero_array, zero_array, zero_array, zero_array, two_array, three_array, three_array, three_array],
-            [zero_array, zero_array, zero_array, zero_array, one_array, two_array, two_array, two_array],
-            [three_array, zero_array, zero_array, zero_array, one_array, two_array, two_array, two_array],
-            [zero_array, three_array, zero_array, zero_array, one_array, one_array, two_array, two_array],
-            [one_array, zero_array, three_array, zero_array, zero_array, one_array, two_array, two_array],
-            [zero_array, three_array, zero_array, three_array, zero_array, one_array, one_array, two_array],
-            [zero_array, zero_array, three_array, zero_array, three_array, zero_array, one_array, one_array],
-            [zero_array, three_array, zero_array, three_array, zero_array, three_array, zero_array, one_array]
+            [0, 0, 0, 0, 2, 3, 3, 3],
+            [0, 0, 0, 0, 1, 2, 2, 2],
+            [3, 0, 0, 0, 1, 2, 2, 2],
+            [0, 3, 0, 0, 1, 1, 2, 2],
+            [1, 0, 3, 0, 0, 1, 2, 2],
+            [0, 3, 0, 3, 0, 1, 1, 2],
+            [0, 0, 3, 0, 3, 0, 1, 1],
+            [0, 3, 0, 3, 0, 3, 0, 1]
         ])
         current_tile = ppu.get_tile_by_name_table_index(0)
+        print(current_tile)
 
         for i in range(0, 8):
             for j in range(0, 8):
-                for channel in range(0, 3):
-                    self.assertEqual(current_tile[i][j][channel], expected_tile[i][j][channel])
+                self.assertEqual(current_tile[i][j], expected_tile[i][j])
