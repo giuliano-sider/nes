8ea706f252658500d05f8511f2cdfe19b9d00fd9
giuliano <giuliano.sider@gmail.com>
2019-11-01 02:00:56 -0300

    Provide a visual test that the attribute table is working by using four different palettes to paint tile at pattern table 0, index 0.

--- nes.py
+++ nes.py
@@ -1,6 +1,6 @@
 from memory_mapper import MemoryMapper
 from cpu import Cpu
-from ppu import Ppu
+from ppu import Ppu, ATTRIBUTE_TABLE_0_ADDRESS
 import numpy as np
 
 
@@ -31,5 +31,8 @@ class Nes():
         self.ppu = Ppu(self.memory_mapper)
         self.memory_mapper.set_cpu(self.cpu)
         self.memory_mapper.set_ppu(self.ppu)
-        nes_image_palette = [0, 0x02, 0x05, 0x0A, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15] # TO BE DELETED
+        
+        nes_image_palette = [0, 0x02, 0x05, 0x0A, 0, 0x07, 0x14, 0x27, 0, 0x29, 0x2C, 0x0D, 0, 0x13, 0x39, 0x1B] # TO BE DELETED
+        for i in range(ATTRIBUTE_TABLE_0_ADDRESS, ATTRIBUTE_TABLE_0_ADDRESS + 64):
+            self.ppu.memory[i] = 0b11100100
         self.given_image_palette(self.ppu, nes_image_palette) # TO BE DELETED
